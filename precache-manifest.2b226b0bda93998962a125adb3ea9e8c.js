cf8c5c1332367ce15bf9467be709907824379ef1
Artyom <litovskys@gmail.com>
2020-04-05 15:19:21 +0300

    Updates

---
+++ precache-manifest.2b226b0bda93998962a125adb3ea9e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a2d20438ab47d5e491d51540f94bbb",
+    "url": "/sceuo-timer/index.html"
+  },
+  {
+    "revision": "9469256188a8b88f89da",
+    "url": "/sceuo-timer/static/css/main.790aca83.chunk.css"
+  },
+  {
+    "revision": "59fb815be8fb7abacca0",
+    "url": "/sceuo-timer/static/js/2.ef07d639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sceuo-timer/static/js/2.ef07d639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9469256188a8b88f89da",
+    "url": "/sceuo-timer/static/js/main.46fc56f8.chunk.js"
+  },
+  {
+    "revision": "68fe30e8710cd9aefee0",
+    "url": "/sceuo-timer/static/js/runtime-main.8f8c43d6.js"
+  }
+]);
